7a201824c177bd86ff6d973b74f85e7f12fb5bce
sanstack256 <nssan2007@gmail.com>
2026-04-08 02:40:35 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -23,3 +23,11 @@ def step(action: str):
         "reward": reward.reward,
         "done": done
     }
+
+def main():
+    import uvicorn
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+
+if __name__ == "__main__":
+    main()
